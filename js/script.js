50a46d529ed36efab57b68c6c38a08e81a695c44
ndambuki94 <ndambuki94@gmail.com>
2019-09-02 09:13:11 +0300

    add function for price determination

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ function Pizza(style, size) {
     } else {
       price+= 20;
     }
+    for (var i = 0; i < this.toppings.length; i++) {
+        price += 1;
+      }
+    
+      this.price = price;
+    }
+    
+    Pizza.prototype.toppingsList = function() {
+      if (this.toppings.length > 0) {
+          return this.toppings.join(", ");
+      } else {
+        return "None";
+      }
+    }
+    
+    $(document).ready(function() {
+      var total = 0;
+      $(".cartTotal").text(total);
+    
+      $("#pizzaForm").submit(function(event) {
+        event.preventDefault();
+        var style = $("#style").val();
+        var size = $("#size").val();
+        var newPizza = new Pizza(style, size);
+    
+        $("input:checkbox[name=topping]:checked").each(function() {
+          var toppingChoice = $(this).val();
+          newPizza.toppings.push(toppingChoice);
+        });
